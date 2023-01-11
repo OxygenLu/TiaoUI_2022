bdf935c0d326283e142a8d65456be59dfb3a8b84
qing-wq <2702461713@qq.com>
2023-01-11 18:35:45 +0800

    添加Material design

--- app.py
+++ app.py
@@ -5,7 +5,22 @@ from PyQt5.QtWidgets import QApplication
 from interface import Window
 from process import Identify
 from client import Client
+from qt_material import apply_stylesheet
+import os
 
+envpath = '~/.local/lib/python3.10/site-packages/cv2/qt/plugins/platforms'
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = envpath
+
+extra = {
+
+    # Button colors
+    'danger': '#dc3545',
+    'warning': '#ffc107',
+    'success': '#17a2b8',
+
+    # Font
+    'font_family': 'Roboto',
+}
 
 class App:
     def __init__(self):
@@ -20,6 +35,7 @@ class App:
         self.identify.start()
         self.client.start()
         if not has_father_window:
+            apply_stylesheet(self.qapp, theme='light_blue.xml', invert_secondary=True, extra=extra)
             sys.exit(self.qapp.exec_())
 
 
